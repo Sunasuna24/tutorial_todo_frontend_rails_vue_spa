2c29c07d37db37bcfd4286c501a686d2d4df47b7
Tomoyasu Sunagawa <tomoyasusunagawa@mac408-sunagawa.local>
2024-05-29 14:02:37 +0900

    投稿画面にフォームを実装した

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,8 +1,29 @@
 <template>
   <h1>This is a post page!</h1>
   <router-link to="home">Go to HOME</router-link>
+  <form @submit.prevent="submit">
+    <div>
+      <label for="title">Title</label>
+      <input type="text" name="title" id="title" />
+    </div>
+    <div>
+      <label for="body">Body</label>
+      <textarea name="body" id="body" cols="30" rows="10"></textarea>
+    </div>
+    <div>
+      <button>Post</button>
+    </div>
+  </form>
 </template>
 
-<script setup>
+<script>
 import { RouterLink } from 'vue-router'
+
+export default {
+  methods: {
+    submit() {
+      //
+    }
+  }
+}
 </script>
