d391c09ec3ac7f3d781b426f83bc49c5f8d6b2ba
Tomoyasu Sunagawa <tomoyasusunagawa@mac408-sunagawa.local>
2024-05-29 15:07:17 +0900

    入力内容を投稿できるようにした

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -4,11 +4,11 @@
   <form @submit.prevent="submit">
     <div>
       <label for="title">Title</label>
-      <input type="text" name="title" id="title" />
+      <input type="text" name="title" id="title" v-model="title" />
     </div>
     <div>
       <label for="body">Body</label>
-      <textarea name="body" id="body" cols="30" rows="10"></textarea>
+      <textarea name="body" id="body" cols="30" rows="10" v-model="body"></textarea>
     </div>
     <div>
       <button>Post</button>
@@ -20,9 +20,33 @@
 import { RouterLink } from 'vue-router'
 
 export default {
+  data() {
+    return {
+      title: '',
+      body: ''
+    }
+  },
   methods: {
     submit() {
-      fetch('http://127.0.0.1:3000/test')
+      if (this.title.trim() === '' || this.body.trim() === '') {
+        console.log('Please fill in all fields')
+        return
+      }
+
+      const requestBody = {
+        title: this.title,
+        body: this.body
+      }
+
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+      }
+
+      fetch('http://127.0.0.1:3000/post', options)
         .then((response) => response.json())
         .then((data) => {
           console.log(data)
@@ -30,6 +54,9 @@ export default {
         .catch((error) => {
           console.error(error)
         })
+
+      this.title = ''
+      this.body = ''
     }
   }
 }
