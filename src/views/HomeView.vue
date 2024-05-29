7e703743ed1e818581c4d9c7572b4d7d51380eb0
Tomoyasu Sunagawa <tomoyasusunagawa@mac408-sunagawa.local>
2024-05-29 16:46:11 +0900

    一覧のタイトルに空欄のリンクをつけた

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <ul>
-    <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    <li v-for="post in posts" :key="post.id">
+      <router-link to="/">{{ post.title }}</router-link>
+    </li>
   </ul>
 </template>
 
