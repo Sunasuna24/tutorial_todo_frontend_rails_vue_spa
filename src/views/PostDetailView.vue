98a58cbfa5f81d608d18a5f1709347e305ff6650
Tomoyasu Sunagawa <tomoyasusunagawa@mac408-sunagawa.local>
2024-05-30 10:57:51 +0900

    idの投稿を取得した

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -1,3 +1,32 @@
 <template>
   <h1>This is Post Detail page.</h1>
+  <p>{{ post_id }}</p>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      post_id: this.$route.params.id,
+      post: {}
+    }
+  },
+  mounted() {
+    this.getPost()
+  },
+  methods: {
+    getPost() {
+      const endPoint = 'http://127.0.0.1:3000/posts/' + this.post_id
+
+      fetch(endPoint)
+        .then((response) => response.json())
+        .then((data) => {
+          this.post = data
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }
+  }
+}
+</script>
