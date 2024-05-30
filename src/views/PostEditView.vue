7f14d02f0baea2a803a104271456cab6f2636028
Tomoyasu Sunagawa <tomoyasusunagawa@mac408-sunagawa.local>
2024-05-30 15:03:13 +0900

    マウントされたらメソッドがフックする雛形を作成した

--- src/views/PostEditView.vue
+++ src/views/PostEditView.vue
@@ -1,3 +1,21 @@
 <template>
   <h1>投稿を編集する</h1>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {}
+    }
+  },
+  mounted() {
+    this.getPost()
+  },
+  methods: {
+    getPost() {
+      console.log('This method was called via mouned hook')
+    }
+  }
+}
+</script>
