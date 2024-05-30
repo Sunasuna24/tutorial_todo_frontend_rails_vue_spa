1a6188e832b37cbc9cfc7c5871da126397bb7221
Tomoyasu Sunagawa <tomoyasusunagawa@mac408-sunagawa.local>
2024-05-30 15:13:38 +0900

    投稿を編集する機能を実装した

--- src/views/PostEditView.vue
+++ src/views/PostEditView.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>投稿を編集する</h1>
-  <form>
+  <form @submit.prevent="editPost">
     <div>
       <label for="title">タイトル</label>
       <input type="text" name="title" id="title" v-model="title" />
@@ -38,6 +38,36 @@ export default {
           this.post = data
           this.title = data.title
           this.body = data.body
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    },
+    editPost() {
+      if (this.title.trim() === '' || this.body.trim() === '') {
+        console.log('Please fill in all fields')
+        return
+      }
+
+      const requestBody = {
+        title: this.title,
+        body: this.body
+      }
+
+      const options = {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+      }
+
+      const endPoint = 'http://127.0.0.1:3000/posts/' + this.post_id
+
+      fetch(endPoint, options)
+        .then((response) => response.json())
+        .then(() => {
+          this.$router.push({ name: 'post-detail', params: { id: this.post.id } })
         })
         .catch((error) => {
           console.error(error)
